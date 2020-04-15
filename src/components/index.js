1be104c5d7154a9fc96d0ea0e72e5fd133d06983
AliAlizada <akbar.alizadeh5@gmail.com>
2020-04-14 18:27:24 -0700

    fix the git package

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
 import Layout from './layout'
 
 export { ProductCarousel } from './ProductComponents/productCarousel'
-export { ProductInfoBox }from './ProductComponents/productInfoBox'
+export { ProductInfoBox }from './ProductComponents/ProductInfoBox'
 export { ProductOverview } from './ProductComponents/productOverview';
 export { BLOCKS, BlogBlocks } from './block-components';
 export { EditToggle } from './edit-toggle'
